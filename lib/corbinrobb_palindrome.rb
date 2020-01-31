15413c85ddf92710b87b6c5abb66d671c1c2ab82
Corbin <corbinrobb@gmail.com>
2020-01-31 10:12:35 -0700

    Initialize repository

---
+++ lib/corbinrobb_palindrome.rb
@@ -0,0 +1,6 @@
+require "corbinrobb_palindrome/version"
+
+module CorbinrobbPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/corbinrobb_palindrome/version.rb
@@ -0,0 +1,3 @@
+module CorbinrobbPalindrome
+  VERSION = "0.1.0"
+end
